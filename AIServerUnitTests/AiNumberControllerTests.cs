9be781719756dbcb2323835390d814d9940aa211
Your Name <futunplay@gmail.com>
2020-11-14 21:05:08 +0300

    Add tests

---
+++ AIServerUnitTests/AiNumberControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AIServerUnitTests
+{
+    public class AiNumberControllerTests
+    {
+
+        public void DefineNumber_Image_SingleNumber()
+        {
+
+        }
+    }
+}
---
+++ AIServerUnitTests/ImageHandlerTest.cs
@@ -0,0 +1,27 @@
+﻿using AIServer.DataHandlers;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace AIServerUnitTests
+{
+    [TestFixture]
+    class ImageHandlerTest
+    {
+        [Test]
+        public void ImageHandler_ImageHandlerCreate_InputDataPropertyHaveAveragePixelsToRGB()
+        {
+            var image = new Bitmap(28, 28);
+
+            for (int y = 0; y < image.Height; y++)
+                for (int x = 0; x < image.Width; x++)
+                {
+                    var newPixel = Color.FromArgb(y + 1, x + 1, x + y);
+                    image.SetPixel(x, y, newPixel);
+                }
+
+            var imageHandler = new ImageHandler(image);
+
+            Assert.AreEqual(imageHandler.InputData.Length, 784);
+        }
+    }
+}
